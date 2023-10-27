15aff753b9c1a960e16831f155e41434ca072e1b
xamanDawud <ahmeddaudking100@gmail.com>
2023-10-28 00:12:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+let users = [
+  { id: 1, name: "Zaman", email: "zaman@gmail.com" },
+  { id: 2, name: "Kaman", email: "kaman@gmail.com" },
+  { id: 3, name: "Daman", email: "daman@gmail.com" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Data is coooooming soooon");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("client data hitting");
+  let newUser = req.body;
+  res.send(newUser);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
